bc735683e1247b4ade36732847a222802c591eb9
alexkad58 <alexkad58@gmail.com>
2021-02-12 17:18:04 +0300

    Workers item

---
+++ itemsCode/worker.js
@@ -0,0 +1,13 @@
+const gagSchema = require("../schemas/gag-schema");
+
+module.exports = async ({ message, args, text, client, prefix, instance, item }) => {
+    const workers = require('./workers.json')
+    let amount = Object.keys(niggers).length
+    function randomInteger(min, max) {
+      // получить случайное число от (min-0.5) до (max+0.5)
+      let rand = min - 0.5 + Math.random() * (max - min + 1);
+      return Math.round(rand);
+    }
+    let key = randomInteger(1, amount)
+    message.reply('Ты взглянул на одного из своих негров.', {files: [workers[key]]})
+}
