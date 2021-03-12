93360b81742cfcfd108b23dbb54ddd283bebbaa2
alexkad58 <alexkad58@gmail.com>
2021-03-12 18:52:21 +0300

    pre

--- commands/rpg-system/drtest.js
+++ commands/rpg-system/drtest.js
@@ -3,10 +3,35 @@ const path = require('path')
 
 module.exports = {
     callback: async ({ message, args, text, client, prefix, instance }) => {
-        server.app.get(`/${message.id}`, (req, res) => {
-        	res.send(`<h1>${message.author.username}</h1>`)
-        })
+        message.react('▶')
+        let party = []
+        party[0] = message.author
+        client.on('messageReactionAdd', async (reaction, user) => {
+            // When we receive a reaction we check if the reaction is partial or not
+            if (reaction.partial) {
+                // If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
+                try {
+                    await reaction.fetch();
+                } catch (error) {
+                    console.error('Something went wrong when fetching the message: ', error);
+                    // Return as `reaction.message.author` may be undefined/null
+                    return;
+                }
+            }
+            if (reaction.emoji.name === '▶') {
+                party.push(user)
+                if (party.length === 3) {
+                    server.app.get(`/${message.id}`, (req, res) => {
+                        res.send(`<h1>${pary[0].username}</h1>
+                        <h1>${pary[1].username}</h1>
+                        <h1>${pary[2].username}</h1>`)
+                    })
+                    
+                    message.reply(`Участники: \n ${pary[0].username} \n ${pary[1].username} \n ${pary[2].username} \n https://LB.alexkad58.repl.co/${message.id}`)
+                }
+                return
+            }
+        });
         
-        message.reply(`https://LB.alexkad58.repl.co/${message.id}`)
     }
 }
