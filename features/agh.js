0169b3f86621bdfff633f6069ae3e01905888cd5
alexkad58 <alexkad58@gmail.com>
2021-05-03 15:17:06 +0300

    agh util

--- features/agh.js
+++ features/agh.js
@@ -1,13 +1,17 @@
 
 module.exports = (client, instance) => {
   client.on('voiceStateUpdate', async (oldMember, newMember)=> {
-    return
-    const cache = {}
+
     const users = ['378767972207951883','298137115877965844']
     if(!oldMember.channelID && newMember.channelID) {
 
-        
-        if (newMember.channel) {
+        let b = false 
+
+        users.forEach(e => {
+            if (e == newMember.member.id) b = true
+        })
+
+        if (newMember.channel && b) {
             newMember.channel.join()
             .then(async connection => {
                 connection.play(`audio/agh.mp3`, { volume: 1});
